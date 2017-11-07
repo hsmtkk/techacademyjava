548ddc55ff4a04072b2b0d7518c43bd2c1ce92a5
hsmtkk <hsmtkk@gmail.com>
2017-11-08 06:50:09 +0900

    kadai8 completed

---
+++ src/kadai8three/Kadai8.java
@@ -0,0 +1,15 @@
+package kadai8three;
+
+public class Kadai8 {
+
+	public static void main(String[] args) {
+		for (int i = 0; i <= 50; i++) {
+			if (i % 3 == 0) {
+				System.out.println(i + "*");
+			} else {
+				System.out.println(i);
+			}
+		}
+	}
+
+}
