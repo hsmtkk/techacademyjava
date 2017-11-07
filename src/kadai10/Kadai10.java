a5714c77f1c7506e32d71381681c7a79edada6ee
hsmtkk <hsmtkk@gmail.com>
2017-11-08 07:02:27 +0900

    kadai10 completed

---
+++ src/kadai10/Kadai10.java
@@ -0,0 +1,13 @@
+package kadai10;
+
+public class Kadai10 {
+
+	public static void main(String[] args) {
+		String[] ss = new String[100];
+		for (int i = 0; i < ss.length; i++) {
+			ss[i] = Integer.toString(i + 1);
+			System.out.println(ss[i]);
+		}
+	}
+
+}
